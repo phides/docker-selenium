85c6802c1244da40be92a77b6f5e6f21d8880c03
phides <39536315+phides@users.noreply.github.com>
2018-05-26 00:02:24 +0200

    Update entry_point.sh

--- Standalone/entry_point.sh
+++ Standalone/entry_point.sh
@@ -19,8 +19,7 @@ SERVERNUM=$(get_server_num)
 
 rm -f /tmp/.X*lock
 
-xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
   ${SE_OPTS} &
 NODE_PID=$!
 
